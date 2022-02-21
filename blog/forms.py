2178ff08568800338c7a8ac296e49bd237768b90
Glo Mon <61246562+GloriaX21@users.noreply.github.com>
2022-02-21 11:27:38 +0100

    Delete forms.py

--- blog/forms.py
+++
@@ -1,9 +0,0 @@
-from socket import forms
-
-class EmailPostForms(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmaiField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
-
-
